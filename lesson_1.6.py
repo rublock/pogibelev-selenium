22af25e2a9df3cbe32f876deef2165a6017a2c1f
mack <hypermail@yandex.ru>
2024-09-11 13:45:44 +0300

    commit

--- lesson_1.6.py
+++ lesson_1.6.py
@@ -1,37 +1,36 @@
-import math
-import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
+# установите пакет chromedrivermanager
+# pip install chromedrivermanager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-link = "http://suninjuly.github.io/find_link_text"
-
 try:
+    link_one = "http://suninjuly.github.io/registration1.html"
+    link_two = "http://suninjuly.github.io/registration2.html"
     browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    browser.get(link)
+    # замените аргумент, чтобы протестировать другую ссылку
+    browser.get(link_one)
 
-    degree = str(math.ceil(math.pow(math.pi, math.e)*10000))
+    first_name = browser.find_element(By.CSS_SELECTOR, "div.first_block div.first_class input.form-control.first")
+    first_name.send_keys("Ivan")
+    last_name = browser.find_element(By.CSS_SELECTOR, "div.first_block div.second_class input.form-control.second")
+    last_name.send_keys("Petrov")
+    email = browser.find_element(By.CSS_SELECTOR, "div.first_block div.third_class input.form-control.third")
+    email.send_keys("example@mail.com")
 
-    link = browser.find_element(By.LINK_TEXT, degree)
-    link.click()
-
-    input1 = browser.find_element(By.TAG_NAME, "input")
-    input1.send_keys("Ivan")
-    input2 = browser.find_element(By.NAME, "last_name")
-    input2.send_keys("Petrov")
-    input3 = browser.find_element(By.CLASS_NAME, "city")
-    input3.send_keys("Smolensk")
-    input4 = browser.find_element(By.ID, "country")
-    input4.send_keys("Russia")
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button = browser.find_element(By.XPATH, "//button[text()='Submit']")
     button.click()
 
-except Exception as error:
-    print(f'Произошла ошибка, вот её трэйсбэк: {error}')
+    time.sleep(1)
+
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    welcome_text = welcome_text_elt.text
+
+    assert "Congratulations! You have successfully registered!" == welcome_text
 
 finally:
-    time.sleep(30)
+    time.sleep(10)
     browser.quit()
