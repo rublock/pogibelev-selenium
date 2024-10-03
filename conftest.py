8a0dd8c38ff05dae0e2bbdc9a6e0c1920821d7be
mack <hypermail@yandex.ru>
2024-10-03 20:08:59 +0300

    firefox

--- conftest.py
+++ conftest.py
@@ -1,12 +1,30 @@
+import logging
+
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='firefox',
+                     help="Choose browser: chrome or firefox")
+
 
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
