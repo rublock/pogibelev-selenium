d6a58502d0b1ee446a8aa8ecb0eba19a1aea03be
mack <hypermail@yandex.ru>
2024-10-06 22:13:52 +0300

    parametrisation test

--- lesson_3_6_test.py
+++ lesson_3_6_test.py
@@ -13,40 +13,32 @@ stepik_password = os.environ.get('STEPIK_PASSWORD')
 
 class TestMainPage1():
     def test_login_into_stepik(self, browser):
-        try:
-            browser.get('https://stepik.org/lesson/236895/step/1')
-            button = (WebDriverWait(browser, 10).until(
-                EC.element_to_be_clickable((By.CLASS_NAME, 'navbar__auth_login'))
-            ))
-            button.click()
+        browser.get('https://stepik.org/lesson/236895/step/1')
 
-            login = (WebDriverWait(browser, 10).until(
-                EC.element_to_be_clickable((By.ID, "id_login_email"))
-            ))
-            login.send_keys(stepik_login)
+        button_login_in = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'navbar__auth_login'))
+        )
+        button_login_in.click()
 
-            browser.find_element(By.ID, "id_login_password").send_keys(stepik_password)
-            browser.find_element(By.CLASS_NAME, "sign-form__btn").click()
+        browser.find_element(By.ID, "id_login_email").send_keys(stepik_login)
+        browser.find_element(By.ID, "id_login_password").send_keys(stepik_password)
+        browser.find_element(By.CLASS_NAME, "sign-form__btn").click()
 
-            answer = math.log(int(time.time()))
+        answer_math = math.log(int(time.time()))
+        time.sleep(3)
+        textarea_math = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "ember-text-area"))
+        )
+        textarea_math.send_keys(answer_math)
 
-            textarea = (WebDriverWait(browser, 10).until(
-                EC.element_to_be_clickable((By.CLASS_NAME, "ember-text-area"))
-            ))
-            textarea.clear()
-            textarea.send_keys(answer)
+        button_submit  = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "submit-submission"))
+        )
+        button_submit.click()
 
-            browser.find_element(By.CLASS_NAME, "submit-submission").click()
-
-            result_text = (WebDriverWait(browser, 10).until(
-                EC.visibility_of_element_located((By.CLASS_NAME, "smart-hints__hint"))
-            ))
-
-            print(result_text.text())
-
-        except Exception as e:
-            print(f"Произошла ошибка: {e}")
-
-        finally:
-            time.sleep(5)
-            browser.quit()
+        result_text = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "smart-hints__hint"))
+        )
+        print(result_text.text)
+        time.sleep(5)
+        browser.quit()
