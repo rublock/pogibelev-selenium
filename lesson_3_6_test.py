4019143ccf9b8735adb2259d8a3e2496a1b8efaf
mack <hypermail@yandex.ru>
2024-10-07 10:01:34 +0300

    parametrisation test

--- lesson_3_6_test.py
+++ lesson_3_6_test.py
@@ -8,15 +8,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-stepik_login = os.environ.get('STEPIK_LOGIN')
-stepik_password = os.environ.get('STEPIK_PASSWORD')
+stepik_login = os.environ.get("STEPIK_LOGIN")
+stepik_password = os.environ.get("STEPIK_PASSWORD")
 
-class TestMainPage1():
+
+class TestMainPage1:
     def test_login_into_stepik(self, browser):
-        browser.get('https://stepik.org/lesson/236895/step/1')
+        browser.implicitly_wait(10)
+        browser.get("https://stepik.org/lesson/236895/step/1")
 
         button_login_in = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'navbar__auth_login'))
+            EC.presence_of_element_located((By.CLASS_NAME, "navbar__auth_login"))
         )
         button_login_in.click()
 
@@ -24,14 +26,13 @@ class TestMainPage1():
         browser.find_element(By.ID, "id_login_password").send_keys(stepik_password)
         browser.find_element(By.CLASS_NAME, "sign-form__btn").click()
 
-        answer_math = math.log(int(time.time()))
         time.sleep(3)
         textarea_math = WebDriverWait(browser, 10).until(
             EC.element_to_be_clickable((By.CLASS_NAME, "ember-text-area"))
         )
-        textarea_math.send_keys(answer_math)
+        textarea_math.send_keys(str(math.log(int(time.time()))))
 
-        button_submit  = WebDriverWait(browser, 10).until(
+        button_submit = WebDriverWait(browser, 10).until(
             EC.element_to_be_clickable((By.CLASS_NAME, "submit-submission"))
         )
         button_submit.click()
